c4b3d54731a3fb8030062c7641ece3136520b212
s20353 <s20353@pjwstk.edu.pl>
2021-11-27 17:09:13 +0100

    Start Screen & Main Menu ready to be filled
    It works tho

--- GameJam-PJATK/Assets/Scripts/MainMenu.cs
+++ GameJam-PJATK/Assets/Scripts/MainMenu.cs
@@ -1,13 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+    public GameObject Instructions;
+    public GameObject Credits;
+    public Dropdown Difficulty;
+
     // Start is called before the first frame update
     void Start()
     {
         gameObject.SetActive(false);
+        
     }
 
     // Update is called once per frame
@@ -15,4 +22,48 @@ public class MainMenu : MonoBehaviour
     {
         
     }
+
+    public void DifficultySelected(Dropdown diff)
+    {
+        int HP; 
+
+        if(diff.value == 0)
+        {
+            HP = 5;
+        }
+        else if(diff.value == 1)
+        {
+            HP = 3;
+        }
+        else
+        {
+            HP = 1;
+        }
+        
+        //tutaj trzeba ustawic HP gracza na te wartosc
+        //GameData.PlayerHP = HP;
+    }
+
+    public void PlayTheGame()
+    {
+        //SceneManager.LoadScene("...");
+    }
+
+    public void GoToInstructions()
+    {
+        gameObject.SetActive(false);
+        Instructions.SetActive(true);
+    }
+
+    public void GoToCredits()
+    {
+        gameObject.SetActive(false);
+        Credits.SetActive(true);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
 }
