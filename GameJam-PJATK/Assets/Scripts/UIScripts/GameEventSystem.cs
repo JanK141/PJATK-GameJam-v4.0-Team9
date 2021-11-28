f489a5f756adcea89b684aa5179b1be10127cb4c
s20353 <s20353@pjwstk.edu.pl>
2021-11-28 02:00:59 +0100

    UI DONE
    Not tested tho

--- GameJam-PJATK/Assets/Scripts/UIScripts/GameEventSystem.cs
+++ GameJam-PJATK/Assets/Scripts/UIScripts/GameEventSystem.cs
@@ -6,7 +6,8 @@ using UnityEngine;
 public class GameEventSystem : MonoBehaviour
 {
     public static GameEventSystem Instance;
-
+    public GameObject UI_canvas;
+    
     [SerializeField]
     private GameData data;
 
@@ -24,9 +25,19 @@ public class GameEventSystem : MonoBehaviour
         data = new GameData();
     }
 
+    public event Action<GameData> OnNewGame; //invoked the moment a new game is started
+
     public event Action<GameData> OnPlayerGetDamage; //invoked on taking damage
     public event Action OnPlayerDead; //invoke on HP reaching 0 in any way
     public event Action OnPlayerFellButSurvived;  //PlayerMovement needs to subscribe a method to bring Player back to last checkpoint on this Event
+
+    public event Action OnGameWon; //invoked when the player reaches the finish
+
+    public void NewGame(GameData data)
+    {
+        Time.timeScale = 1;
+        OnNewGame?.Invoke(data);
+    }
 
     //this gets called whenever a player takes damage
     public void PlayerGetDamage()
@@ -37,6 +48,7 @@ public class GameEventSystem : MonoBehaviour
         if (DED)
         {
             OnPlayerDead?.Invoke();
+            //Time.timeScale = 0;
         }
     }
 
@@ -51,5 +63,12 @@ public class GameEventSystem : MonoBehaviour
         {
             OnPlayerDead?.Invoke();
         }
+    }
+
+    //this should be called from the Finish object, when the Player reaches it
+    public void GameWon()
+    {
+        OnGameWon?.Invoke();
+        //Time.timeScale = 0;
     }
 }
