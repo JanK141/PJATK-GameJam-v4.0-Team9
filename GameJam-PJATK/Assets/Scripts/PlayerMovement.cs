589147befe28cd48bee3b9025b9e745ddf969381
krajewss <pszenica99@gmail.com>
2021-11-28 03:25:48 +0100

    mały commit

--- GameJam-PJATK/Assets/Scripts/PlayerMovement.cs
+++ GameJam-PJATK/Assets/Scripts/PlayerMovement.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody))]
@@ -9,6 +10,8 @@ public class PlayerMovement : MonoBehaviour
     float movementSpeed, jumpForce;
 
     Rigidbody rb;
+    private Animator anim;
+    private Collider collider;
     float movement = 0f;
     bool isGrounded = true;
     bool jump = false;
@@ -17,12 +20,15 @@ public class PlayerMovement : MonoBehaviour
 
     void Awake()
     {
+        collider = GetComponent<Collider>();
         rb = GetComponent<Rigidbody>();
+        anim = transform.GetChild(0).GetComponent<Animator>();
         normalJumpForce = jumpForce;
     }
 
     void Update()
     {
+        anim.SetFloat("velocity", rb.velocity.magnitude);
         float x = Input.GetAxis("Horizontal");
         float y = Input.GetAxis("Vertical");
         movement = x * movementSpeed;
@@ -42,14 +48,19 @@ public class PlayerMovement : MonoBehaviour
                 jump = true;
             }
         }
+
+        transform.LookAt(new Vector3(transform.position.x +rb.velocity.normalized.x*10, transform.position.y, transform.position.z));
     }
 
     void FixedUpdate()
     {
+
+
         rb.velocity = new Vector3(movement, rb.velocity.y);
 
         if (jump)
         {
+            anim.SetTrigger("jump");
             Jump();
             jump = false;
         }
@@ -58,7 +69,7 @@ public class PlayerMovement : MonoBehaviour
     void Jump()
     {
         rb.AddForce(jumpForce * Vector3.up, ForceMode.Impulse);
-        isGrounded = false;
+        //isGrounded = false;
     }
 
     void OnCollisionEnter(Collision collision)
@@ -67,6 +78,16 @@ public class PlayerMovement : MonoBehaviour
         {
             isGrounded = true;
             secondJumpUsed = false;
+            anim.SetBool("Grounded", true);
+        }
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
+            anim.SetBool("Grounded", false);
         }
     }
 }
